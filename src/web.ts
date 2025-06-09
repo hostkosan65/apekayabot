f4d73913b98879afa4708366874d86a59741b1c4
hostkosan65 <hostkosan65@gmail.com>
2025-06-10 00:35:48 +0700

    Update web.ts

--- src/web.ts
+++ src/web.ts
@@ -1,17 +1,14 @@
-import HTTP from 'node:http';
+import http from 'node:http';
 
-const PORT = process.PORT || 5500;
-const server = HTTP.createServer((request, response) => {
-	response.writeHead(200, {
-		"Access-Control-Allow-Origin": "https://replit.com",
-		"Access-Control-Allow-Methods": "GET, PING, OPTIONS",
-		"Content-Type": "text/html"
-	} as const);
-	response.end("<h3>Copy me, the url above!</h3>");
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("✅ AFKBot is alive!");
 });
 
-
-
 export default (): void => {
-	server.listen(PORT, () => console.log("Server for UptimeRobot is ready!"));
+  server.listen(PORT, "0.0.0.0", () =>
+    console.log(`Web server running at port ${PORT}`)
+  );
 };
