a2086bea5dce0f05ca03d6b7f4e6fcec72c1dd50
hostkosan65 <hostkosan65@gmail.com>
2025-06-10 00:50:33 +0700

    Update web.ts

--- src/web.ts
+++ src/web.ts
@@ -1,6 +1,6 @@
 import http from 'node:http';
 
-const PORT = process.env.PORT || 10000;
+const PORT = Number(process.env.PORT) || 5500;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
